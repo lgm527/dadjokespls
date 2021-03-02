dbd6e0b8b0a6a3d6ee0651eb4c782d4129faee6c
lgm527 <laurellmccaffrey@gmail.com>
2021-03-01 20:48:10 -0500

    dad jokes please

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import DadJoke from './DadJoke';
 
 function App() {
+
+  const [joke, setJoke] = useState('');
+  const [color, setColor] = useState('');
+
+  const randomHex = () => {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++ ) {
+       color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  const fetchDadJoke = () => {
+    fetch(`https://icanhazdadjoke.com/`, {
+      headers: {
+        Accept: 'application/json'
+      }
+    })
+    .then(res => res.json())
+    .then(data => {
+      setJoke(data.joke);
+    })
+    .catch(err => {
+      console.log(err);
+      setJoke('Oh no! something went wrong, 😞 dad jokes unavailable right now');
+    })
+  }
+
+  const handleClick = ()  => {
+    fetchDadJoke();
+    setColor(randomHex());
+  }
+
+  useEffect(() => {
+    fetchDadJoke();
+    setColor(randomHex());  
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App" style={{backgroundColor: color}}>
+      <header className="App-content">
+        Welcome to your infinite supply of dad jokes!
+        <br></br>
+        ∞ 👨 💬
       </header>
+
+      <DadJoke joke={joke}/>
+
+      <button onClick={handleClick}>One Fresh Dad Joke Please</button>
+
     </div>
   );
 }
---
+++ src/DadJoke.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function DadJoke(props) {
+
+     return (
+       <div className="joke">
+       <p>{props.joke}</p>
+       </div>
+     );
+
+ }
+
+ export default DadJoke;
